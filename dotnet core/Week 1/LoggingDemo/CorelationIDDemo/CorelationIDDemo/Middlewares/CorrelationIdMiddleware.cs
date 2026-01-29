35bdd690e984b5a9374e8174eb1e3789f64682d7
Dakshil Gorasiya <Dakshil.g@dev.rkitsoft.local>
2026-01-29 16:46:32 +0530

    Add Logging demo

---
+++ dotnet core/Week 1/LoggingDemo/CorelationIDDemo/CorelationIDDemo/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CorelationIDDemo.Middlewares
+{
+    public class CorrelationIdMiddleware
+    {
+        public const string HeaderName = "X-Correlation-ID";
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CorrelationIdMiddleware> _logger;
+
+        public CorrelationIdMiddleware(
+            RequestDelegate next,
+            ILogger<CorrelationIdMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var correlationId =
+                context.Request.Headers[HeaderName].FirstOrDefault()
+                ?? Guid.NewGuid().ToString();
+
+            context.Items[HeaderName] = correlationId;
+
+            context.Response.Headers[HeaderName] = correlationId;
+
+            using (_logger.BeginScope(new Dictionary<string, object>
+            {
+                ["CorrelationId"] = correlationId
+            }))
+            {
+                await _next(context);
+            }
+        }
+    }
+}
